8fcd892e8ab319e117e35322b692a8e138a5ea75
debSneha <snehadebsikdar@gmail.com>
2024-07-02 22:25:49 +1000

    init

---
+++ cultured-app-client/jest.config.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+    // The root of your source code, typically /src
+    // `<rootDir>` is a token Jest substitutes
+    roots: ["<rootDir>/src"],
+
+    // Jest transformations -- this adds support for TypeScript
+    // using ts-jest
+    transform: {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+
+    // Test spec file resolution pattern
+    // Matches parent folder `__tests__` and filename
+    // should contain `test` or `spec`.
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+
+    // Module file extensions for importing
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+    testEnvironment: 'jsdom',
+
+    moduleNameMapper: {
+        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.cjs",
+        "\\.(css|sass)$": "identity-obj-proxy",
+    },
+
+    setupFilesAfterEnv: [
+        "<rootDir>/jest.setup.cjs"]
+};
